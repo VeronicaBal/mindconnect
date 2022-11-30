3bcba90381d90ca6dcc5c2ffed4d4a328b9d0e24
Veronica <veronica_baldin@live.it>
2022-11-30 13:06:04 +0100

    Removed index route

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var userRouter = require('./routes/user');
 var dataRouter = require('./routes/data');
 var tracked_itemsRouter = require('./routes/tracked_items');
@@ -20,7 +19,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/client/build')));
 
-app.use('/', indexRouter);
 app.use('/user', userRouter);
 app.use(dataRouter);
 app.use(tracked_itemsRouter);
